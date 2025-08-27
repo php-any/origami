885ef43a341f7f73cba3bbc9588f83287ba30ca1
lwlwilliam <lwlwilliam@foxmail.com>
2025-08-28 00:01:52 +0800

    fix bool parser

--- parser/bool_parser.go
+++ parser/bool_parser.go
@@ -1,51 +1,54 @@
 package parser
 
 import (
-	"github.com/php-any/origami/data"
-	"github.com/php-any/origami/node"
-	"github.com/php-any/origami/token"
+    "fmt"
+
+    "github.com/php-any/origami/data"
+    "github.com/php-any/origami/node"
+    "github.com/php-any/origami/token"
 )
 
 // BoolParser 表示bool类型声明解析器
 type BoolParser struct {
-	*Parser
+    *Parser
 }
 
 // NewBoolParser 创建一个新的bool类型声明解析器
 func NewBoolParser(parser *Parser) StatementParser {
-	return &BoolParser{
-		parser,
-	}
+    return &BoolParser{
+        parser,
+    }
 }
 
 // Parse 解析bool类型声明
 func (p *BoolParser) Parse() (data.GetValue, data.Control) {
-	// 开始位置跟踪
-	tracker := p.StartTracking()
+    // 开始位置跟踪
+    tracker := p.StartTracking()
 
-	// 跳过bool关键字
-	p.next()
+    // 跳过bool关键字
+    p.next()
+    fmt.Println(p.current().Literal)
 
-	// 检查下一个token是否是变量
-	if p.checkPositionIs(0, token.VARIABLE, token.IDENTIFIER) {
-		from := tracker.End()
-		return nil, data.NewErrorThrow(from, data.NewError(from, "bool类型声明需要变量名", nil))
-	}
+    // 检查下一个token是否是变量
+    if !p.checkPositionIs(0, token.VARIABLE, token.IDENTIFIER) {
+        from := tracker.End()
+        return nil, data.NewErrorThrow(from, data.NewError(from, "bool类型声明需要变量名", nil))
+    }
 
-	// 获取变量名
-	varName := p.current().Literal
-	p.next()
+    // 获取变量名
+    varName := p.current().Literal
+    p.next()
 
-	// 结束位置跟踪，获取准确的From信息
-	from := tracker.EndBefore()
+    // 结束位置跟踪，获取准确的From信息
+    from := tracker.EndBefore()
 
-	// 在作用域中添加变量
-	val := p.scopeManager.CurrentScope().AddVariable(varName, data.Bool{}, from)
+    // 在作用域中添加变量
+    val := p.scopeManager.CurrentScope().AddVariable(varName, data.Bool{}, from)
 
-	// 创建变量表达式
-	expr := node.NewVariableWithFirst(from, val)
+    // 创建变量表达式
+    expr := node.NewVariableWithFirst(from, val)
 
-	// 解析后续操作（函数调用、数组访问等）
-	vp := &VariableParser{p.Parser}
-	return vp.parseSuffix(expr)
+    // 解析后续操作（函数调用、数组访问等）
+    vp := &VariableParser{p.Parser}
+    return vp.parseSuffix(expr)
 }
