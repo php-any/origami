f9baa382f980b9950ca31ffd74d6b0c6e7919554
ctfang <1992053348@qq.com>
2026-09-20 21:38:47 +0800

    1

--- examples/laravel13/storage/framework/views/65e153c36ee0ecb02c0ca363d60d6d768678c9b44b93f5e424de19967c90a3de.php
+++
@@ -1,8 +0,0 @@
-<section
-    <?php echo e($attributes->merge(['class' => "w-full max-w-7xl mx-auto p-4 sm:p-14 border-x border-dashed border-neutral-300 dark:border-white/[9%]"])); ?>
-
->
-    <?php echo e($slot); ?>
-
-</section>
-<?php /**PATH d:\gitcode.com\origami\examples\laravel13\vendor\laravel\framework\src\illuminate\foundation\providers/../resources/exceptions/renderer/components/section-container.blade.php ENDPATH**/ ?>
--- examples/laravel13/storage/framework/views/d69f74d28d1520143b9283921bffec72a3e71d10afd3063a5b010fdcb24fbe72.php
+++
@@ -1,8 +0,0 @@
-<div
-    <?php echo e($attributes->gridColumn($this->getColumnSpan(), $this->getColumnStart())->class(['fi-wi-widget'])); ?>
-
->
-    <?php echo e($slot); ?>
-
-</div>
-<?php /**PATH D:\gitcode.com\origami\examples\laravel13\vendor\filament\widgets\resources\views/components/widget.blade.php ENDPATH**/ ?>
